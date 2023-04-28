97cfd1d0dbe481b0a848cf6f3471bbbe51969d91
pedronogs <phnogs@hotmail.com>
2023-04-28 22:39:24 +0000

    feat: add navbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,110 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav id="navbar">
+    <div id="logo-container">
+      <img src="@/assets/logo.png" />
+      Nicole.
+    </div>
+    <div id="links">
+      <router-link to="/works">Trabalhos</router-link>
+      <router-link to="/about">Sobre</router-link>
+      <router-link to="/contact">Contato</router-link>
+    </div>
   </nav>
-  <router-view />
+  <div id="wrapper">
+    <router-view />
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+$nav-font-color: #CCCCCC
+$nav-link-font-hover: #CCCCCC
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+@font-face
+  font-family: 'Montserrat'
+  src: url("@/assets/fonts/montserrat/montserrat-variable.ttf")
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+*
+  font-family: 'Montserrat', sans-serif
+
+html, body
+  width: 100%
+  height: 100%
+  margin: 0
+  padding: 0
+
+#app
+  width: 100%
+  height: 100%
+
+#wrapper
+  width: 100%
+  height: 100%
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
+#navbar
+  position: fixed
+
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  align-items: center
+
+  height: 3rem
+  width: 100%
+
+  background-color: #024A59
+
+#logo-container
+  display: flex
+  flex-direction: row
+  justify-content: center
+  align-items: center
+
+  margin-left: 5rem
+
+  height: 90%
+
+  color: $nav-font-color
+  font-weight: 700
+  font-size: 1.5rem
+
+  img
+    height: 90%
+
+    object-fit: cover
+
+#links
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  align-items: center
+
+  width: 20%
+
+  margin-right: 5rem
+
+  a
+    padding: 0.5rem
+    padding-right: 1rem
+    padding-left: 1rem
+
+    border-radius: 1rem
+
+    color: $nav-font-color
+
+    font-size: 1.3rem
+    text-decoration: none
+
+  a:hover
+    background-color: black
+
+    -webkit-transition: all 0.75s ease-in
+    -moz-transition: all 0.75s ease-in
+    -o-transition: all 0.75s ease-in
+    -ms-transition: all 0.75s ease-in
+    transition: all 0.75s ease-in
 </style>
