255835ac3ad8cb10915cb8ad4be073050eb369cb
pedronogs <phnogs@hotmail.com>
2023-05-03 01:33:17 +0000

    feat: UI works section and footer

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -11,7 +11,10 @@
     </section>
 
     <section id="uxui-works">
-      <span id="header"> Esses são alguns dos meus trabalhos de UX/UI: </span>
+      <span id="header">
+        Esses são alguns dos meus trabalhos de
+        <span class="orange-underline">UX/UI:</span>
+      </span>
       <div class="item">
         <div class="item-text-content">
           <span>Nara Rubens</span>
@@ -33,9 +36,54 @@
 
     <section id="ui-works">
       <span id="header">
-        E aqui tem alguns dos meus trabalhos apenas de UI:
+        E aqui tem alguns dos meus trabalhos
+        <span class="orange-underline">apenas de UI:</span>
       </span>
-      <div id="item"></div>
+      <div class="item"></div>
+    </section>
+
+    <section id="about">
+      <span id="header">
+        E se você quiser saber um pouco mais
+        <span class="orange-underline">sobre mim:</span>
+      </span>
+
+      <div id="background-container"></div>
+
+      <div id="character-image">
+        <img src="@/assets/character.png" />
+      </div>
+
+      <div id="content">
+        <p>
+          Tenho formação e pós-graduação em psicologia e atualmente venho me
+          especializando na área do UX/UI design.
+        </p>
+        <p>
+          O interesse surgiu a partir do desejo de maior liberdade criativa. Ter
+          a possibilidade de criar interfaces, trabalhar com elementos visuais
+          sendo algo que me traz imensa curiosidade e me leva a constante busca
+          por conhecimento, sempre me impulsionando a buscar inovação.
+        </p>
+        <p>
+          Procuro unir este meu lado que preza pelo visual, ao lado o qual
+          investi muito estudo e apreço que é a psicologia buscando o bem-estar
+          e qualidade de vida do indivíduo e usuário levando a importantes
+          características de um bom ux/ui designer.
+        </p>
+      </div>
+    </section>
+
+    <section id="footer">
+      <div id="header">
+        Se quiser entrar em <span class="orange-underline">contato:</span>
+      </div>
+      <div id="content">
+        <div class="item"><button>Behance</button></div>
+        <div class="item"><button>LinkedIn</button></div>
+        <div class="item"><button>nicolexavierp@gmail.com</button></div>
+        <div class="item"><button>Currículo</button></div>
+      </div>
     </section>
   </div>
 </template>
@@ -181,12 +229,15 @@ $base-color-grey: #CCCCCC
     background-color: black
 
 #ui-works
-  height: 50vh
+  height: 60vh
+
+  padding-bottom: 7vh
 
   background-color: $base-color-green
 
   display: flex
   flex-direction: column
+  align-items: center
 
   #header
     margin-top: 10%
@@ -198,4 +249,137 @@ $base-color-grey: #CCCCCC
     color: $base-color-grey
     font-weight: 600
     font-size: 1.4rem
+
+  .item
+    margin-top: 2rem
+
+    width: 80%
+    height: 80%
+
+    background-color: $base-color-grey
+
+#about
+  height: 90vh
+
+  padding-bottom: 7vh
+
+  position: relative
+
+  display: flex
+  flex-direction: column
+  align-items: center
+
+  #header
+    margin-top: 10%
+
+    font-size: 1.4rem
+    font-weight: 600
+    color: $base-color-green
+
+    align-self: center
+
+    text-align: center
+
+  #background-container
+    position: absolute
+
+    height: 85%
+    width: 90%
+
+    border-top-right-radius: 10rem
+    border-top-left-radius: 10rem
+
+    bottom: 0
+
+    background-color: #B3C3C0
+
+    z-index: -999
+
+  #character-image
+    margin-top: 15%
+
+    img
+      width: 9rem
+      height: 9rem
+
+      object-fit: contain
+
+  #content
+    position: relative
+
+    width: 80%
+    height: 60%
+
+    margin: 2rem
+
+    border: solid 0.125rem $base-color-orange
+    border-left: none
+
+    font-size: 0.875rem
+    font-weight: 400
+    text-indent: 10px
+    text-align: justify
+    color: $base-color-green
+
+    p
+      padding: 0 1rem 0 1rem
+
+  #content::before
+    content: ""
+    position: absolute
+
+    left: 0px
+    bottom: 0px
+
+    height: 50%
+    width: 0.125rem
+
+    background: $base-color-orange
+
+#footer
+  height: 20vh
+  width: 100%
+
+  background-color: $base-color-green
+
+  display: flex
+  flex-direction: column
+  align-items: center
+
+  #header
+    margin-top: 10%
+
+    font-size: 1.2rem
+    font-weight: 600
+    color: $base-color-grey
+
+  #content
+    margin-top: 1rem
+
+    display: grid
+    grid-template-columns: repeat(2, 1fr)
+    justify-content: center
+    align-items: center
+
+    text-align: center
+
+    .item
+      font-size: 0.75rem
+      font-weight: 600
+
+      padding: 0.5rem
+
+      button
+        color: $base-color-grey
+
+        cursor: pointer
+
+        background: none
+        border: none
+
+      button:hover
+        background-color: black
+
+.orange-underline
+  border-bottom: solid 0.125rem $base-color-orange
 </style>
